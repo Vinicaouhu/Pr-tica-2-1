989b76a91af971aa7d99ec00db44e4a46aba6fd4
Vinícius Almeida <>
2021-04-12 14:36:55 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  float a=2;//modificar por qualquer valor real
+  float b=3;//modificar por qualquer valor real
+  float c=4;//modificar por qualquer valor real
+  float media=a*b*c/3;
+  printf("%f\n",media);
+  return 0;
+}
